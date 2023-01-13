45518a67198637689fa2567fd71055f3aa1c37ee
Miha <ati2lova3@gmail.com>
2023-01-14 00:10:32 +0200

    Tested Serial2 from mega

--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,51 @@
 #include <Arduino.h>
 #include "App.h"
 #include "Config.h"
-// #include <UniversalTelegramBot.h>
+#include "Secret.h"
 
-// UniversalTelegramBot bot(BOT_TOKEN, client);
+// Изменено в lib/FastBot/FastBot.h
+// Original: void(*_callback)(FB_msg& msg)
+// Now: #define SMART_CALLBACK std::function<void(FB_msg& msg)> _callback
 
-//Serial2 ESP32 порт в мегу 
-#define RXD2 16    // 
-#define TXD2 17    // 
+#define RXD2 16
+#define TXD2 17
 
 App a(conf);
-String ttydata = "";
+String ttyData;
 
 void setup() {
-  Serial.begin(115200);
-  Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2); //порт обмена с  мегой
-  
-  
-   a.run();
-}
+    Serial.begin(115200);
+    Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2); //порт обмена с  мегой
+
+    a.run();
+};
+
+void readMegaSerial()
+  {
+        constexpr static const byte RESPONSE_END = '%',
+                                    SKIP_LINE = '\r';
+
+        while(Serial2.available())
+            {
+                char nextCharacter = Serial2.read();
+                //delay (1);
+
+                // Если перевод в начало внутри текста, пропускаем его, чтобы не было еще одной пустой строки
+                if(nextCharacter == SKIP_LINE)
+                    continue;
+
+                if(nextCharacter == RESPONSE_END)
+                    {
+                        Serial.println("Встретился %, выводим в телегу");
+                        a.transferToTelegram(ttyData);
+                        ttyData.clear();
+                    };
+
+
+                ttyData += nextCharacter;
+            };
+    };
 
 void loop() {
-  // while(Serial2.available()) 
-  //   {
-  //     // Serial.println("Available");
-  //     String r = Serial2.readStringUntil('%');
-  //     Serial.printf("Received from mega -> %s\n", r);
-  //     a.transferToTelegram(r);
-
-  //   }
-
-
-
-    while ( Serial2.available() > 0)
-      { //Serial.println("Есть что принимать, принпроверяем наличие новых сообщений от меняимаем");
-        char c = Serial2.read();
-        //delay (1);
-
-        //разбор принятого символа
-        if (c == '\r') //если перевод в начало внутри текста, пропускаем его, чтобы не было еще одной пустой строки
-        { }  // пропускаем вхождящий символ
-        else 
-          if (c == '%') //если встретился символ конца выводимого буфера, отдаем весь буфер в Телеграм
-            {   Serial.println ("Встретился %, выводим в телегу");
-              // ttydata += c;
-              //flag_buf = 1; //              
-              
-              if(!ttydata.isEmpty())
-                a.transferToTelegram(ttydata);
-              ttydata = "";
-            }
-            else //иначе добавляем этот символ в стринг
-              ttydata += c;
-
-       }// while
-
-
-}
-
+  readMegaSerial();
+};
