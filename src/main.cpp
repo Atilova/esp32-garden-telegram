743c0153c5a622d8742102069b9d6471b1dfda73
Miha <ati2lova3@gmail.com>
2023-01-17 22:11:19 +0200

    main.cpp cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -13,12 +13,12 @@ App a(conf);
 #define RXD2 16
 #define TXD2 17
 
-void setup() 
+void setup()
     {
         Serial.begin(115200);
         Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);
         a.run();
-    };  
+    };
 
 void loop() {
 
